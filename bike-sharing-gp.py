d423410cf54d868a9d743f723162353cb1b55934
Soumyasmruti <soumyasmruti@gmail.com>
2015-05-12 04:07:13 -0600

    Final COmmit

--- bike-sharing-gp.py
+++ bike-sharing-gp.py
@@ -2,6 +2,8 @@
 from __future__ import print_function
 import numpy as np
 import googleprediction as gp
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+
 
 model = gp.GooglePredictor(
     "vital-form-93916",
@@ -15,6 +17,7 @@ model.fit('CLASSIFICATION')
 
 model.get_params()
 
-X_train = np.genfromtxt('final_test.csv', delimiter=',', skip_header=1)
+X_test = np.genfromtxt('val_test.csv', delimiter=',', skip_header=1)
 
-out = model.predict(X_train)
+result = model.predict(X_test)
+
--- bike_pred.py
+++ bike_pred.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import math
 from sklearn.metrics import mean_absolute_error
 from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
 
 date_str = '%Y-%m-%d %H:%M:%S'
@@ -36,7 +37,7 @@ def load_data():
     train = process_new_attributes(train)
     for col in ['casual', 'registered', 'count']:
         train['log-' + col] = train[col].apply(lambda x: math.log(1 + x))
-    
+        
     y_train = np.asarray(train.count) 
     X_train = train.drop(["count"],axis =1)
     
@@ -58,29 +59,51 @@ def merge_predict(model1, model2, test_data):
     p_total = (p1+p2)
     return(p_total)
      
-            
+        
 def train_and_predict():
     print "training..."
-    training, y_train, X_test = load_data()
+    X_train, y_train, X_test = load_data()
     
+    training = X_train[X_train.day > 3]
+    validation = X_train[X_train.day <= 3]
     
-    
-    est_casual = ensemble.RandomForestRegressor(n_estimators=1000, n_jobs=-1, min_samples_split=11, oob_score=True, verbose=1)
-    est_registered = ensemble.GradientBoostingRegressor(n_estimators=80, learning_rate = .05)
-    param_grid2 = {'max_depth': [6],
+    est_casual = ensemble.RandomForestRegressor(n_estimators=1000, n_jobs=-1, min_samples_split=6, oob_score=True, verbose=1)
+    est_registered = ensemble.GradientBoostingRegressor(n_estimators=100, learning_rate = .05)
+    param_grid2 = {'max_depth': [6, 10, 15, 20],
                 'min_samples_leaf': [3, 5, 10, 20],
                 }
     
-    gs_casual = GridSearchCV(est_casual, param_grid2, n_jobs=-1).fit(training[features], training['log-casual'])
-    gs_registered = GridSearchCV(est_registered, param_grid2, n_jobs=-1).fit(training[features], training['log-registered'])      
+    gs_casual = GridSearchCV(est_casual, param_grid2, n_jobs=-1).fit(X_train[features], X_train['log-casual'])
     
-    result3 = merge_predict(gs_casual,  gs_registered, X_test[features])
-    df=pd.DataFrame({'datetime':X_test['datetime'], 'count':result3})
+    print gs_casual.best_score_
+    
+    gs_registered = GridSearchCV(est_registered, param_grid2, n_jobs=-1).fit(X_train[features], X_train['log-registered']) 
+    
+    print gs_registered.best_score_
+    
+    error_count_casual = mean_absolute_error(np.float64(validation['log-casual']), np.float64(gs_casual.predict(validation[features])))
+    error_count_regs = mean_absolute_error(np.float64(validation['log-registered']), np.float64(gs_registered.predict(validation[features])))
+    
+    print "Mean Absolute Error when predicting for on Casual Users: "+ str(error_count_casual)
+    print "Mean Absolute Error when predicting for on Registered Users: "+ str(error_count_regs)
+         
+    
+    result = merge_predict(gs_casual,  gs_registered, X_test[features])
+    
+    df=pd.DataFrame({'datetime':X_test['datetime'], 'count':result})
     df.to_csv('output.csv', index = False, columns=['datetime','count']) 
-    training = training.drop(["datetime"],axis =1)
-    X_test = X_test.drop(["datetime"],axis=1)
+    
+         
+    training = training.drop(training.columns[[0,1]],axis =1)
+    validation =validation.drop(validation.columns[[0,1]],axis =1)
+    X_test = X_test.drop(X_test.columns[[0,1]],axis=1)
+    
+    'For running it for google prediction we do the following steps'
+    
     training.to_csv('final_train.csv', sep=',')
+    validation.to_csv('validate.csv', sep=',')
     X_test.to_csv('final_test.csv', sep=',')  
+    
     
     
 #def gen_submission():
